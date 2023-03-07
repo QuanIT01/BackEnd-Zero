2ade27251ea4eadffb3c14852bc214a22414ef86
QuanIT01 <n.h.quan126@gmail.com>
2023-03-07 21:25:31 +0700

    more routes

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/a", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/b", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
