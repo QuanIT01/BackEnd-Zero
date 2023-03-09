162d37f01351453befcd2e55e77a7415af48c7f8
QuanIT01 <n.h.quan126@gmail.com>
2023-03-09 14:26:20 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require("express"); // commonjs
 const path = require("path");
+require("dotenv").config();
 const app = express(); // app express
-const port = 8081; //port
+
+//console.log(">>> check env: ", process.env);
+const port = process.env.PORT || 8888; //port
+const hostname = process.env.HOST_NAME;
 
 // config template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+//config static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // khai bao route
 app.get("/", (req, res) => {
@@ -21,6 +28,6 @@ app.get("/b", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
