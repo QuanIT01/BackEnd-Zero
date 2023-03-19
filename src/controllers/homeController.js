b6f694c29f07949ae3920c80bee7c5a42c0706c8
QuanIT01 <n.h.quan126@gmail.com>
2023-03-19 20:46:23 +0700

    display list user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,7 +9,7 @@ const {
 const User = require("../models/user");
 
 const getHomePage = async (req, res) => {
-  let results = [];
+  let results = await User.find({});
   return res.render("home.ejs", { listUsers: results });
 };
 
@@ -24,12 +24,6 @@ const postCreateUser = async (req, res) => {
   let email = req.body.email;
   let name = req.body.myname;
   let city = req.body.city;
-  console.log(">>> email: ", email, "name =", name, "city = ", city);
-
-  // let [results, fields] = await connection.query(
-  //   `INSERT INTO Users (email, name, city) VALUES (? , ? , ?)`,
-  //   [email, name, city]
-  // );
   await User.create({
     email: email,
     name: name,
