36c3e88b939076fc29843eae5df185a974b24cd8
QuanIT01 <n.h.quan126@gmail.com>
2023-03-25 08:14:03 +0700

    setup storage file with nodejs

--- src/server.js
+++ src/server.js
@@ -3,11 +3,16 @@ const express = require("express"); // commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const apiRoutes = require("./routes/api");
+const fileUpload = require("express-fileupload");
 const connection = require("./config/database");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; //port
 const hostname = process.env.HOST_NAME;
+
+//config fileupload
+// default options
+app.use(fileUpload());
 
 //config req.body
 app.use(express.json()); // Used to parse JSON bodies
