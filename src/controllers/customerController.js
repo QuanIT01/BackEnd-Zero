3d4271f1946debde1717a310fe7baa1da4b06f32
QuanIT01 <n.h.quan126@gmail.com>
2023-04-03 22:55:57 +0700

    query buider advance

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -52,9 +52,10 @@ module.exports = {
   getAllCustomers: async (req, res) => {
     let limit = req.query.limit;
     let page = req.query.page;
+    let name = req.query.name;
     let result = null;
     if (limit && page) {
-      result = await getAllCustomerService(limit, page);
+      result = await getAllCustomerService(limit, page, name, req.query);
     } else {
       result = await getAllCustomerService();
     }
